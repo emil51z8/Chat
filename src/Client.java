b996eb33450b69cc60f713fab53f7332fbc8b8df
KevinDMort <kevi5221@edu.zealand.dk>
2023-09-15 21:00:26 +0200

    Gået væk fra clienthandler igen. fandt noget som hedder en executor som er en meget bedre løsning. Med denne laver vi en pool af tråde som severen styrer. Har lavet noget basis kode for at implementere chat protokollen // ikke testet.
    Har lavet en test og klient kan forbinde til server og får tildelt et SessionID, andet er ikke testet og er nok fyldt med fejl

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+import java.io.*;
+import java.net.*;
+
 public class Client {
-
-    private String sessionID;
-
+    private static String sessionID = "";
 
     public static void main(String[] args) {
+        String serverAddress = "localhost";
+        int serverPort = 1992;
 
+        try (
+                Socket socket = new Socket(serverAddress, serverPort);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            System.out.println("Connected to server: " + serverAddress + ":" + serverPort);
+
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String serverMessage;
+                    while ((serverMessage = in.readLine()) != null) {
+                        if (sessionID.isEmpty() && serverMessage.substring(0, 3).equals("999")) {
+                            sessionID = serverMessage.substring(3, 7);
+                            System.out.println("Received sessionID: " + sessionID);
+                        } else if (sessionID.isEmpty() && serverMessage.substring(0, 3).equals("000"))
+                        {
+                            // Vælg nyt brugernavn
+                        }
+                        else{
+                            // Imp+ement chat here
+                            System.out.println("Server: " + serverMessage);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            receiveThread.start();
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+
+                out.println(userInput);
+                if (userInput.equalsIgnoreCase("exit")) {
+                    // send quit besked til server
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,64 +1,69 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.*;
+import java.util.concurrent.*;
 
-public class
-Server {
+public class Server {
 
-    private static HashMap<String, User> connectedUsers = new HashMap<>();
+    private static ConcurrentHashMap<String, User> connectedUsers = new ConcurrentHashMap<>();
+    private static ExecutorService executor = Executors.newFixedThreadPool(20); // Change the pool size as needed
 
     public void addUser(String SID, Socket clientSocket) {
-
-        synchronized (connectedUsers) {
-            User user = new User("", clientSocket);
-            connectedUsers.put(SID, user);
+        User user = new User("", clientSocket);
+        connectedUsers.put(SID, user);
+    }
+    public void sendMessageToUser(String SID, String message, String receiverSID) {
+        User reciever = connectedUsers.get(receiverSID);
+        User sender = connectedUsers.get(SID);
+        if (reciever != null) {
+            reciever.sendMessage("PM Fra: " +sender.getUsername()+ ": " + message);
         }
     }
-
-    public void sendMessageToUser(String username, String message) {
-        synchronized (connectedUsers) {
-            User user = connectedUsers.get(username);
-            if (user != null) {
-                user.sendMessage(message);
+    public void sendBroadcastMessage(String name, String message) {
+        for (Map.Entry<String, User> entry : connectedUsers.entrySet()) {
+            User user = entry.getValue();
+            user.sendMessage(name+": " +message);
+        }
+    }
+    public void updateName(String SID, String name)
+    {
+        connectedUsers.get(SID).setUsername(name);
+    }
+    public boolean isUsernameTaken(String username) {
+        for (Map.Entry<String, User> entry : connectedUsers.entrySet()) {
+            User user = entry.getValue();
+            if (user.getUsername().equals(username)) {
+                return true; // Username er taget
             }
         }
+        return false; // Username er ledigt
     }
 
-    private void readInput(String input) {
+    private Boolean readInput(String input) {
         String status = input.substring(0, 3);
-        if (status.equals("100")) {
-
+        String SID = input.substring(3,7);
+        if (status.equals("100"))
+        {
+           String name = input.substring(7);
+           if (isUsernameTaken(name) == false) {
+               updateName(SID, name);
+           }
+           else return true;
         }
         if (status == "200") {
+            String msg = input.substring(7);
+            sendBroadcastMessage(connectedUsers.get(SID).getUsername(),msg);
         }
         if (status == "300") {
-            //kald metode sendPrivateMessage
+            String reciever = input.substring(7,11);
+            String msg = input.substring(11);
+            sendMessageToUser(SID,msg,reciever);
         }
         if (status == "400") {
-            //kald metode quit
+            sendBroadcastMessage("Server :", connectedUsers.get(SID).getUsername() + " har forladt chatten");
+           connectedUsers.remove(SID);
         }
-    }
-
-    public String createUser(String navn, Socket socket) {
-        String result = "";
-        for (Map.Entry<String, User> entry : connectedUsers.entrySet()) {
-            User value = entry.getValue();
-            if (value.getUsername().equals(navn.trim())) {
-                return "";
-            }
-        }
-        User u = new User(navn, socket);
-        for (int i = 1000; i <= 9999; i++)
-        {
-            if (connectedUsers.containsKey(i+"") == false) {
-                result = i + "";
-                break;
-            }
-        }
-        connectedUsers.put(result,u);
-
-            return result;
+        return false;
     }
     public static String findFreeSID()
     {
@@ -70,9 +75,9 @@ Server {
                 break;
             }
         }return SID;
- }
+    }
     public static void main(String[] args) {
-        Server theSever = new Server();
+        Server theServer = new Server();
 
         int portNumber = 1992;
         ServerSocket serverSocket = null;
@@ -84,11 +89,34 @@ Server {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket.getInetAddress());
                 String SID = findFreeSID();
-                // Create a new thread to handle the client
-                ClientHandler clientHandler = new ClientHandler(clientSocket,SID);
+                theServer.addUser(SID,clientSocket);
 
-                Thread thread = new Thread(clientHandler);
-                thread.start();
+                executor.submit(() -> {
+                    try (
+                            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                    )
+                    {
+                        out.println("999"+SID);
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            Boolean taget = theServer.readInput(inputLine);
+                            if(taget = true)
+                            {
+                                out.print(000); // Brugernavn er taget Client skal vælge et andet
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            clientSocket.close();
+                            System.out.println("Closed connection from " + clientSocket.getInetAddress());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -102,9 +130,4 @@ Server {
             }
         }
     }
-
-
-    }
-
-
-
+}
