3b4054fec66bc80c7cfcadaaaace1c78faf06c0e
Jakob F <Jakobfoersom88@gmail.com>
2023-09-18 11:18:13 +0200

    Tjek brugernavn
    Jeg har lavet en metode til at tjekke at brugernavnet ikke indeholder mellemrum og @.
    Men når man laver kalder metoden Tjekbrugernavn, i en tråd registere den ikke det første input men det andet input.

--- src/Client.java
+++ src/Client.java
@@ -11,27 +11,43 @@ import java.util.Scanner;
 public class Client {
     private static String sessionID = "";
 
-    static Scanner input = new Scanner(System.in);
+
 
     private static String tjekBrugernavn()
     {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Opret brugernavn");
+        System.out.println("Brugernavn må ikke indholde mellemrum eller @");
         String brugernavn = input.nextLine();
-        for(int i = 0; i < brugernavn.length(); i ++){
-            if(brugernavn.indexOf("@" && brugernavn.indexOf(" "){}
-        }
+        
 
+
+        boolean containsWhiteSpace = brugernavn.contains(" ");
+        boolean containsElefantNoseA = brugernavn.contains("@");
+
+        if(containsWhiteSpace || containsElefantNoseA){
+            System.out.println("Der må ikke være mellemrum eller @ i dit brugernavn.");
+            System.out.println("Prøv igen");
+            brugernavn = "";
+            tjekBrugernavn();
+        }
+        System.out.println("Du har fået brugernavn " + brugernavn );
+        return brugernavn;
     }
     private static void clientMenu(){
         if(sessionID.equals("999"))
         {
-
+            String valgteBrugernavn = tjekBrugernavn();
+            System.out.println(valgteBrugernavn);
         }
     }
 
     public static void main(String[] args) {
         String serverAddress = "localhost";
         int serverPort = 1992;
+        tjekBrugernavn();
 
+        //tjekBrugernavn();
         try (
                 Socket socket = new Socket(serverAddress, serverPort);
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -46,14 +62,18 @@ public class Client {
                     while ((serverMessage = in.readLine()) != null) {
                         if (sessionID.isEmpty() && serverMessage.substring(0, 3).equals("999")) {
                             sessionID = serverMessage.substring(3, 7);
+
+                            //tjekBrugernavn();
+
                             System.out.println("Received sessionID: " + sessionID);
                         } else if (sessionID.isEmpty() && serverMessage.substring(0, 3).equals("000"))
                         {
                             // Vælg nyt brugernavn
+                            //tjekBrugernavn();
+
                         }
                         else{
                             // Imp+ement chat here
-
                             System.out.println("Server: " + serverMessage);
                         }
                     }
